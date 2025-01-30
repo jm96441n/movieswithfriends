8b69f01e5d129566444a2cdc8b92bda1e25defbe
jm96441n <john@jmaguire.tech>
2025-01-29 22:28:10 -0500

    starting to setup kamal

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -60,7 +60,33 @@ resource "digitalocean_volume_attachment" "app" {
   ]
 }
 
+# Create a new container registry
+resource "digitalocean_container_registry" "registry" {
+  name                   = "jmaguireregistry"
+  subscription_tier_slug = "starter"
+  region                 = "nyc2"
+}
+
+# Create a container registry docker credentials
+resource "digitalocean_container_registry_docker_credentials" "registry_creds" {
+  registry_name = digitalocean_container_registry.registry.name
+  write         = true # Set to true for read/write access, false for read-only
+}
+
 # outputs.tf
 output "vps_ip" {
   value = digitalocean_droplet.app.ipv4_address
+}
+
+output "registry_endpoint" {
+  value = digitalocean_container_registry.registry.endpoint
+}
+
+output "registry_server_url" {
+  value = digitalocean_container_registry.registry.server_url
+}
+
+output "registry_credentials" {
+  value     = digitalocean_container_registry_docker_credentials.registry_creds.docker_credentials
+  sensitive = true # Marks the output as sensitive to prevent credentials from showing in logs
 }
